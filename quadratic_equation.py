7541c8878d5bd8ea3d521bdf1b4338066655a0f4
Sir Nightmare <ivantochilkin@gmail.com>
2017-01-03 17:45:58 +0300

    function optimized

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -6,7 +6,8 @@ def get_roots(a, b, c):
     if discriminant < 0:
         return None, None
     if discriminant == 0:
-        return -b / (2 * a), None
+        root1 = -b / (2 * a)
+        return root1, None
     else:
         root1 = (-b - sqrt(discriminant)) / (2 * a)
         root2 = (-b + sqrt(discriminant)) / (2 * a)
